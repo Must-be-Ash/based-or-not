c02585ec96d46223a961338bd4316931950320c9
Must-be-Ash <arash.nouruzi@yahoo.com>
2025-04-03 17:07:19 -0700

    manifest update

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,22 +1,24 @@
 export async function GET() {
   const URL = process.env.NEXT_PUBLIC_URL;
 
-  return Response.json({
-    accountAssociation: {
-      header: process.env.FARCASTER_HEADER,
-      payload: process.env.FARCASTER_PAYLOAD,
-      signature: process.env.FARCASTER_SIGNATURE,
-    },
-    frame: {
-      version: process.env.NEXT_PUBLIC_VERSION,
-      name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
-      homeUrl: URL,
-      iconUrl: process.env.NEXT_PUBLIC_ICON_URL,
-      imageUrl: process.env.NEXT_PUBLIC_IMAGE_URL,
-      buttonTitle: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME}`,
-      splashImageUrl: process.env.NEXT_PUBLIC_SPLASH_IMAGE_URL,
-      splashBackgroundColor: `#${process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR}`,
-      webhookUrl: `${URL}/api/webhook`,
-    },
-  });
+  return Response.json(
+    {
+      "accountAssociation": {
+        "header": "eyJmaWQiOjI0MjQzNCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDhmMUJBYjZBNkM4MjAxZTc0OTFlQzM2QzcxMUQ4MThmMEQ1OGYzNkIifQ",
+        "payload": "eyJkb21haW4iOiJ0aW1lZHJpZ2h0LnZlcmNlbC5hcHAifQ",
+        "signature": "MHhkMzczY2U2ZDhiYWVjZTY1ODg5ZjZlZjgwZDk2MDUyMzk4YTc2NWI3NmQzMTYzYzZiNmRmZmU2YTBhMDFmYmFmNDYxN2E5MDgyNWRkYzM0ZjlkZGJhMWE1MGIyMzQxNjExZDI0ZjY2MzJmODE5MmVkMjhjZTY3MDZhNGU0YjkyZTFi"
+      },
+      "frame": {
+        "version": "next",
+        "name": "timed-right",
+        "homeUrl": "https://timedright.vercel.app/",
+        "iconUrl": "https://timedright.vercel.app//chad.png",
+        "imageUrl": "https://timedright.vercel.app//chad.png",
+        "buttonTitle": "Launch timed-right",
+        "splashImageUrl": "https://timedright.vercel.app//chad.png",
+        "splashBackgroundColor": "#FFFFFF",
+        "webhookUrl": "https://timedright.vercel.app//api/webhook"
+      },
+    }
+  );
 }
